bb66e8da9a7ccd5ec182cf3b4e163547cd124216
Idris Olubisi <heedris2olubisi@gmail.com>
2022-02-17 12:05:09 +0100

    Added Chains

---
+++ src/utils/chains.js
@@ -0,0 +1,39 @@
+const Chains = [
+  {
+    key: "0x89",
+    name: "Polygon",
+    value: "polygon",
+  },
+  {
+    key: "0x1",
+    name: "Ethereum Mainnet",
+    value: "mainnet",
+  },
+  {
+    key: "0xa86a",
+    name: "Avalanche",
+    value: "avalanche",
+  },
+  {
+    key: "0x38",
+    name: "Binance",
+    value: "binance",
+  },
+  {
+    key: "0x3",
+    name: "Ropsten Testnet",
+    value: "ropsten",
+  },
+  {
+    key: "0x2a",
+    name: "Kovan Testnet",
+    value: "kovan",
+  },
+  {
+    key: "0x5",
+    name: "Goerli Testnet",
+    value: "goerli",
+  },
+];
+
+export default Chains;
